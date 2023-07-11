5ad8f7fa453a00fda4404874c2a9fcb266c91fdd
hataylor <datahattrick@gmail.com>
2023-07-11 16:01:43 +1200

    Removed JSON reader from get handler, not required

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -42,19 +42,6 @@ func (apiCfg *apiConfig) handlerCreateFeedFollows(w http.ResponseWriter, r *http
 
 func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
 
-	type parameters struct {
-		FeedID uuid.UUID `json:"feed_id"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %s", err))
-		return
-	}
-
 	feedFollow, err := apiCfg.DB.GetFeedFollow(r.Context(), user.ID)
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't get feed follows: %s", err))
